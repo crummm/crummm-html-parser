7375ec210f7702ff60885a0bfae221e753c22f0e
crummm <toddmz+github@gmail.com>
2018-05-01 16:14:09 -0500

    vue working example pointing at the 'dist' directory

---
+++ examples/vue/vue-2.x-ts/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { MmmHtmlParser } from '../../../../dist/index';
+
+export default Vue.extend({
+  name: 'app',
+  created() {
+    new MmmHtmlParser().start();
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
